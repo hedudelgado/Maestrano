4941f29e619ffdda4be0e743333e7f6744d4f0f0
hedudelgado <hedu.delgado@gmail.com>
2016-05-17 01:05:04 +0100

    api spits the two most important work locations and the most importat sales channel

--- Api_test/app/services/employee_locations_services.rb
+++ Api_test/app/services/employee_locations_services.rb
@@ -1,13 +1,12 @@
 require 		'location.rb'
 require_relative'urls_http_requests.rb'
+require 'top_employee_locations_selector.rb'
 
 def call_employee_work_locations_api
 	auth = {:username=>ENV["USERNAME"], :password=>ENV["PASSWORD"]}
   employee_locations = HTTParty.get(employee_work_Location_url , :basic_auth => auth)
   serialize_work_location(employee_locations) 
 end
-
-private 
 
 def serialize_work_location(employee_locations)
   employee_locations= JSON.parse(employee_locations)
@@ -17,4 +16,5 @@ def serialize_work_location(employee_locations)
   	location = Location.new(attributes["latitude"],attributes["longitude"],attributes["description"],attributes["name"])
    	@locations.push(location)
  	end
+ 	employee_locations_selector(@locations)
 end	
--- Api_test/app/services/sales_flow_service.rb
+++ Api_test/app/services/sales_flow_service.rb
@@ -11,8 +11,8 @@ end
 
 def serialize_sales_flow_location(sales_flow)
   @sales_location = []
- 	
  	sales_flow["content"]["customers"].each do |sales|
+
     @sales_location.push(Sale.new(sales["name"],sales["country"],sales["city"],sales["total_invoiced"]))
   end
   top_sales_location_selector(@sales_location)
