07080989d4214bc0ddfaaf934f38444bb5ca6d5e
hedudelgado <hedu.delgado@gmail.com>
2016-05-30 17:22:41 +0100

    environments configuration gem and its urls variables added

--- Api_test/app/services/api_calls.rb
+++ Api_test/app/services/api_calls.rb
@@ -1,11 +1,23 @@
-require 'urls_http_requests.rb'
+require 'top_sales_location_selector.rb'
+require 'serialize_sales_flow_location.rb'
+
 class ApiCalls
 
 	def call_employee_work_locations_api
 		auth = {:username=>ENV["USERNAME"], :password=>ENV["PASSWORD"]}
-	  employee_locations = HTTParty.get(employee_work_Location_url , :basic_auth => auth)
+	  employee_locations = HTTParty.get(Settings.employee_work_Location_url , :basic_auth => auth)
 	  serializer = EmployeeSerializer.new
 	  serializer.serialize_work_location(employee_locations)
 	end	
+
+	def call_sales_flow_api
+		p 'aaaaaaaaaaaaa'
+	auth = {:username=>ENV["USERNAME"], :password=>ENV["PASSWORD"]}
+  sales_flow = HTTParty.get(Settings.url_sales_flow, :basic_auth => auth)    
+  p 'aaaaaaaaaaaaa'
+  p sales_flow
+  #p'-----------------'
+  serialize_sales_flow_location(sales_flow) 
+end
 
 end
