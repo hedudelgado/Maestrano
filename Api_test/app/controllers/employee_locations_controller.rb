f14868caf8229b96be672a93d031d80d5610bc03
hedudelgado <hedu.delgado@gmail.com>
2016-06-01 22:15:48 +0100

    rails bug, spit jason either logged or not

--- Api_test/app/controllers/employee_locations_controller.rb
+++ Api_test/app/controllers/employee_locations_controller.rb
@@ -2,7 +2,6 @@ require 'json'
 
 class EmployeeLocationsController < ApplicationController
   before_action :set_employee_location, only: [:show, :edit, :update, :destroy]
-
   # GET /call_employee_work_locations_api
   # GET /employee_locations.json
   def index
